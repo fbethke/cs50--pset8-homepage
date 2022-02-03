cecb1dcbb8f87c62ee361dd8d69ae4783a979a51
FBethke <86567665+fbethke@users.noreply.github.com>
2022-02-03 23:56:00 +0100

    turn_around function added

--- my_javascript.js
+++ my_javascript.js
@@ -7,3 +7,16 @@ $(function () {
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+var check = 0;
+
+function turn_around() { 
+  if (check == 0){
+    document.getElementsByClassName("timeline")[0].style.flexDirection = "column-reverse";
+    check = 1;
+  }
+  else {
+    document.getElementsByClassName("timeline")[0].style.flexDirection = "column";
+    check = 0;
+  }
+}
